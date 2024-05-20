c077db38d6e404730c92c8c2e6d05adccf81b5e4
TUDNYT <nykol_yove.armacanqui_martinez_de_tudor@mailbox.tu-dresden.de>
2024-05-20 14:41:24 +0200

    :memo: Milestone 2 V1

--- R/1. load/Kenya_Ghana_WealthQHH.R
+++ R/1. load/Kenya_Ghana_WealthQHH.R
@@ -1,11 +1,41 @@
 # Reading the DHS Wealth Index Quantile Household Questionnaire
 
+# loading packages
 library(haven)
+library(tidyverse)
+library(tidyr)
+library(dplyr)
+library(ggplot2)
+
+# Reading and importing the csv file
 wealthqhh <- read_csv("~/Documents/TUD/TUD 2024 S2/Empirical Research Task/Ghana/DHS/Ghana Kenya /Household WealthHQQ/idhs_00003.csv")
 print(wealthqhh)
 
 # Selecting only the columns WEALTHQHH and YEAR
 
-library(dplyr)
-wealthqhh <- data.frame(wealthqhh) %>% select(WEALTHQHH, YEAR)
+wealthqhh <- data.frame(wealthqhh) %>% select(WEALTHQHH, YEAR, COUNTRY)
 print(wealthqhh)
+
+# Then, dropping the NA values
+
+wealthqhh_clean <- na.omit(wealthqhh)
+print(wealthqhh_clean)
+
+# Export this file as CSV. for labanya 
+
+write.csv(wealthqhh_clean, "wealthqhh_clean.csv", row.names = FALSE)
+
+# filter the data so I can just focus on Ghana data. 
+
+wealthqhh_ghana <- wealthqhh_clean %>% filter(COUNTRY == 288)
+print(wealthqhh_ghana)
+
+# Remove the country column frmo the dataframe 
+
+wealthqhh_ghana <- wealthqhh_ghana %>% select(-COUNTRY)
+print(wealthqhh_ghana)
+
+
+
+
+
