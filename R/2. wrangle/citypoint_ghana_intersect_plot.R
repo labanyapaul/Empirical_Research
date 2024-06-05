16259c4d5f07b3c59d9f53c35430ec3e841a73bf
TUDNYT <nykol_yove.armacanqui_martinez_de_tudor@mailbox.tu-dresden.de>
2024-06-06 01:21:16 +0200

    :memo: ghana landfills treatment/control group 2014

--- R/2. wrangle/citypoint_ghana_intersect_plot.R
+++ R/2. wrangle/citypoint_ghana_intersect_plot.R
@@ -9,13 +9,11 @@ library(units)
 library(readr)
 library(ggplot2)
 library(ggspatial)
-
 library(knitr)
 library(kableExtra)
 
 
-library(readr)
-Ghana_landfill_city_intersects <- read_csv("R/2. wrangle/Ghana_landfill_city_intersects.csv")
+Ghana_landfill_city_intersects <- read_csv("./output/Ghana_landfill_city_intersects.csv")
 View(Ghana_landfill_city_intersects)
 
 desired_crs <- 2136
@@ -37,4 +35,45 @@ ggplot() +
   theme_void() +
   ggspatial::annotation_scale() +
   ggtitle("Spatial Data for Accra with City Center Point")
+
+# Filter cities: 2. Kumasi
+
+kumasi_data <- intersecting_cities %>%
+  filter(cty_name == "Kumasi")
+  
+print(kumasi_data)
+
+# Calculate centroid of Kumasi polygon
+kumasi_center <- kumasi_data %>%
+  st_centroid()
+  
+# Plot Kumasi data with city center point
+ggplot() +
+  geom_sf(data = kumasi_data) + 
+  geom_sf(data = kumasi_center, color = "black", size = 3) + 
+  theme_void() +
+  ggspatial::annotation_scale() +
+  ggtitle("Spatial Data for Kumasi with City Center Point")
+
+# Filter cities: 3. Bolgatanga
+
+bolgatanga_data <- intersecting_cities %>%
+  filter(cty_name == "Bolgatanga")
+
+print(bolgatanga_data)
+
+# Calculate centroid of Bolgatanga polygon
+bolgatanga_center <- bolgatanga_data %>%
+  st_centroid()
+
+# Plot Bolgatanga data with city center point
+ggplot() +
+  geom_sf(data = bolgatanga_data) + 
+  geom_sf(data = bolgatanga_center, color = "black", size = 3) + 
+  theme_void() +
+  ggspatial::annotation_scale() +
+  ggtitle("Spatial Data for Bolgatanga with City Center Point")
+
+
+
 
