c7e11ace964b20500b770365838f6c132832e206
labanyapaul <labanya.paul@mailbox.tu-dresden.de>
2024-06-19 14:55:04 +0200

    :memo:

---
+++ R/3. analysis/Regression Kenya.R
@@ -0,0 +1,41 @@
+library(readr)
+combined_dataKibera <- here::here("output//combined_dataKibera.csv")
+combined_dataKibera <- read_csv("output/combined_dataDandora.csv")
+View(combined_dataKibera)
+
+
+combined_dataDandora <- here::here("output//combined_dataDandora.csv")
+read_csv(combined_dataDandora)
+combined_dataDandora <- read_csv("output//combined_dataDandora.csv")
+View(combined_dataDandora)
+
+# Update the ADM1NAME column in combined_dataDandora
+combined_dataDandora <- combined_dataDandora %>%
+  mutate(ADM1NAME = ifelse(ADM1NAME == "Nairobi", "NairobiD", ADM1NAME))
+
+
+
+combined_dataMombasa <- here::here("output//combined_dataMombasa.csv")
+read_csv(combined_dataMombasa)
+combined_dataMombasa <- read_csv("output/combined_dataMombasa.csv")
+View(combined_dataMombasa)
+
+combined_dataKenya <- rbind(combined_dataKibera, combined_dataDandora, combined_dataMombasa)
+
+# View the combined dataframe
+View(combined_dataKenya)
+
+library(plm)
+
+# Create dummy variable for Group
+combined_dataKenya$Treatment <- ifelse(combined_dataKenya$Group == "Treatment", 1, 0)
+
+# Ensure the data is in the right format for plm
+combined_dataKenya <- pdata.frame(combined_dataKenya, index = c("ADM1NAME"))
+
+# Run the fixed effects model
+model_kenya <- plm(WEALTHQHH~ Treatment, data = combined_dataKenya, model = "within")
+
+# Summarize the results
+summary(model_kenya)
+
