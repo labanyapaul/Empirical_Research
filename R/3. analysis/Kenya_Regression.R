35cb456b82913ba5aa02041136fd1e21348bb531
labanyapaul <labanya.paul@mailbox.tu-dresden.de>
2024-06-25 14:35:32 +0200

    :memo:

--- R/3. analysis/Kenya_Regression.R
+++ R/3. analysis/Kenya_Regression.R
@@ -9,6 +9,7 @@ read_csv(combined_dataDandora)
 combined_dataDandora <- read_csv("output//combined_dataDandora.csv")
 View(combined_dataDandora)
 
+library(dplyr)
 # Update the ADM1NAME column in combined_dataDandora
 combined_dataDandora <- combined_dataDandora %>%
   mutate(ADM1NAME = ifelse(ADM1NAME == "Nairobi", "NairobiD", ADM1NAME))
@@ -28,7 +29,6 @@ write_csv(combined_dataKenya, "output/combined_dataKenya.csv")
 # View the combined dataframe
 View(combined_dataKenya)
 
-library(plm)
 
 # Create dummy variable for Group
 combined_dataKenya$Treatment <- ifelse(combined_dataKenya$Group == "Treatment", 1, 0)
@@ -37,15 +37,29 @@ combined_dataKenya$Treatment <- ifelse(combined_dataKenya$Group == "Treatment", 
 combined_dataKenya <- pdata.frame(combined_dataKenya, index = c("ADM1NAME"))
 
 # Run the fixed effects model
-model_kenya <- plm(WEALTHQHH~ Treatment, data = combined_dataKenya, model = "within")
-
+library(fixest)
+model_kenya <- fixest::feols(WEALTHQHH ~ Treatment| ADM1NAME, data = combined_dataKenya)
 # Summarize the results
 summary(model_kenya)
 
-#Regression table for model_kenya
+#Regression table for model_kenya using etable and save the table in output folder
 
-library(stargazer)
-stargazer(model_kenya, type = "text")
+library(etable)
 
-# Save the regression table
-stargazer(model_kenya, type = "html", out = "output/model_kenya.html")
+
+# Load necessary libraries
+library(fixest)
+library(here)
+
+# Fit the model
+model_kenya <- fixest::feols(WEALTHQHH ~ Treatment | ADM1NAME, data = combined_dataKenya)
+
+# Generate the regression table
+Reg_tableKenya <- etable(model_kenya)
+
+#save the regression table
+write.table(Reg_tableKenya, "output/reg_tableKenya.txt", row.names = FALSE, col.names = TRUE, quote = FALSE, sep = "\t")
+
+# View the regression table
+View(Reg_tableKenya)
+
